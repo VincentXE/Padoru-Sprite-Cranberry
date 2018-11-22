cf467134b7b61de207ea8ede0553380bb10a9981
Vincent <vinlark@outlook.com>
2018-11-22 11:13:55 -0600

    Cleaned up code

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import os
 import winsound
 import subprocess
 #drive = "C:\\"
-folder = "lib"
-image = "image.png"
-image_path = os.path.join(folder, image)
 SPI_SETDESKWALLPAPER = 20 
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "lib/image.png", 3)
 while True:
 	winsound.PlaySound('lib/padoru.wav', winsound.SND_FILENAME)
