2cfa4da5ace24a799b8da2fbb08201acee6db08a
Vincent <vinlark@outlook.com>
2018-11-23 15:26:46 -0600

    this took way too long

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import ctypes, os, winsound
-SPI_SETDESKWALLPAPER = 20 
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "lib/image.png", 3)
+import ctypes, os, winsound, win32con, PIL
+from PIL import Image
+
+Image = Image.open('lib\image.png')
+Image.show()
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "lib/image.bmp", 0)
+winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
 while True:
-	winsound.PlaySound('lib/padoru.wav', winsound.SND_FILENAME)
+	winsound.PlaySound('lib\padoru.wav', winsound.SND_FILENAME)
