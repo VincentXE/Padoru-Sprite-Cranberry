c38bd9e894add75754bbe3c36a05ca37adef432f
Vincent <vinlark@outlook.com>
2018-11-23 19:49:47 -0600

    while true: fuck up

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import ctypes, os, winsound, win32con, PIL
 from PIL import Image
 
 Image = Image.open('lib\image.png')
-Image.show()
+#Image.show()
 ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "lib/image.bmp", 0)
-winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
+#winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
 while True:
+	Image.show()
 	winsound.PlaySound('lib\padoru.wav', winsound.SND_FILENAME)
+	winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
+
