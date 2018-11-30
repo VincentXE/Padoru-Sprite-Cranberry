761aa7e1c5f7e706b42059bf12dc0dc84c3f3574
Vincent <vinlark@outlook.com>
2018-11-30 17:37:45 -0600

    Added notepad.exe lol

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import ctypes, os, winsound
 ctypes.windll.user32.SystemParametersInfoA(20, 0, "lib/image.png", 0)
-#while True:
+while True:
 	#winsound.PlaySound('lib\padoru.wav', winsound.SND_FILENAME)
 	#winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
 	os.startfile('lib\\pandoru.png')
 	os.startfile('lib\\image.png')
 	os.startfile('lib\\spritecranberry.png')
+	os.startfile('notepad.exe')
 
