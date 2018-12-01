1d6ba948a4c5566db234a5c13cca027f7a5dcd1b
Vincent <vinlark@outlook.com>
2018-11-30 19:05:55 -0600

    For I have made death, the white horse.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import ctypes, os, winsound
+import ctypes, os, winsound, subprocess
 ctypes.windll.user32.SystemParametersInfoA(20, 0, "lib/image.png", 0)
+p = subprocess.Popen(["lib\\res.exe",
+				'/x:800',
+				'/y:600'])
+p.communicate()
 while True:
-	#winsound.PlaySound('lib\padoru.wav', winsound.SND_FILENAME)
-	#winsound.PlaySound('lib\spritecranberry.wav', winsound.SND_FILENAME)
+	winsound.PlaySound('lib\\padoru.wav', winsound.SND_FILENAME)
 	os.startfile('lib\\pandoru.png')
 	os.startfile('lib\\image.png')
 	os.startfile('lib\\spritecranberry.png')
